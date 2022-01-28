c68081a8f31bc9a39b218379493575737a9c5e4e
Nikolai Kirnosoff <kalun62@gmail.com>
2022-01-28 13:34:22 +0300

    lesson02

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-let  title,
-     screens,
-     screenPrice,
-     rollback,
-     fullPrice,
-     adaptive;
+let  title = 'glo-academy-course-JS',
+     screens = 'Простые, Сложные, Интерактивные',
+     screenPrice = 1276,
+     rollback = 56,
+     fullPrice = 3000000,
+     adaptive = true;
 
-alert('hello');
+console.log(typeof title);
 
-console.log('hy');
+console.log(typeof fullPrice);
+
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log('Стоимость верстки экранов' + ' ' + screenPrice + ' '  + 'рублей/ долларов/гривен/юани');
+
+console.log('Стоимость разработки сайта' + ' ' + fullPrice + ' '  + 'рублей/ долларов/гривен/юани');
+
+console.log(screens.toLowerCase().split(', '));
+
+console.log('Процент отката посреднику за работу' + ' ' + (fullPrice * (rollback/100)));
+
